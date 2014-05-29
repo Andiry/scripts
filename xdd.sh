3e184981724affcacc7fdf4949b81fdba6e06f15
Andiry Xu <jix024@cs.ucsd.edu>
2014-05-28 23:34:59 -0700

    xdd scripts fix

--- xdd.sh
+++ xdd.sh
@@ -11,11 +11,13 @@ FILE_RANGE=$3
 THREAD=$4
 RWRATIO=$5
 
+FILE_RANGE=$((${FILE_RANGE}*1024*1024/${REQ_SIZE}))
+
 echo "xdd: Workload ${WORKLOAD_SIZE}MB, Reqsize ${REQ_SIZE}, File range ${FILE_RANGE}MB, Thread ${THREAD}, RWratio ${RWRATIO}"  
 
 XDDEXEC=~/benchmarks/xdd/bin/xdd.linux
 
-XDDFLAGS="-mbytes ${WORKLOAD_SIZE} -minall -dio -verbose -noproclock -nomemlock -runtime 0 -reqsize 1 -blocksize ${REQ_SIZE} -timelimit 0 -seek random -seek range $[${FILE_RANGE}*1024*1024/${REQ_SIZE}] -seek seed 333 -queuedepth ${THREAD}"
+XDDFLAGS="-mbytes ${WORKLOAD_SIZE} -minall -verbose -noproclock -nomemlock -runtime 0 -reqsize 1 -blocksize ${REQ_SIZE} -timelimit 0 -seek random -seek range ${FILE_RANGE} -seek seed 333 -queuedepth ${THREAD}"
 
 ${XDDEXEC} ${XDDFLAGS} -targets 1 /mnt/ramdisk/test1 -rwratio ${RWRATIO}
 
